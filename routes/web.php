140f254a82fdc608d3d9d66e0e3a115316dff714
bruryansah <bruryansah@gmail.com>
2024-10-16 07:33:34 -0700

    first

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('Error', function () {
+    return view('404');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('feature', function () {
+    return view('feature');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('project', function () {
+    return view('project');
+});
+
+Route::get('quote', function () {
+    return view('quote');
+});
+
+Route::get('service', function () {
+    return view('service');
+});
+
+Route::get('team', function () {
+    return view('team');
+});
+
+Route::get('testimonial', function () {
+    return view('testimonial');
+});
